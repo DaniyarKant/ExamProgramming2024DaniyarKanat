59dae6e081f5048647b48c671bd01c17b635bdeb
Daniyar <d.kanat1@mail.ru>
2024-02-14 19:14:21 -0300

    counting

--- Utilities/Class1.cs
+++ Utilities/Class1.cs
@@ -8,5 +8,18 @@ namespace Utilities
 {
     public static class StringUtilities
     {
+        public static int LongWordsCount(string input)
+        {
+            string[] words = input.Split(' ');
+            int count = 0;
+            foreach (string word in words)
+            {
+                if (word.Length > 2)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
     }
 }
