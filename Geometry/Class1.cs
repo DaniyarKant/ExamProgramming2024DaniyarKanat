c68d3bf2ee026cc1b3c6e4f6ea4a72d79599d44a
Daniyar <d.kanat1@mail.ru>
2024-02-14 18:50:24 -0300

    class point

--- Geometry/Class1.cs
+++ Geometry/Class1.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace Geometry
 {
-    public class Class1
+    public class Point
     {
+        public decimal X { get; }
+        public decimal Y { get; }
+
+        public Point(decimal x)
+        {
+            X = x;
+            Y = 0;
+        }
+
+        public Point(decimal x, decimal y)
+        {
+            X = x;
+            Y = y;
+        }
     }
 }
